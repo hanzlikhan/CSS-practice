9948ae6d1ef130edbc6908210221837777bf64ee
hanzlikhan <khangormani79@gmail.com>
2024-03-06 23:57:52 +0500

    init

---
+++ simple-portfolio/script.js
@@ -0,0 +1,7 @@
+// Add JavaScript functionality here, if needed
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // You can add form submission logic here, like sending the data to a server
+    alert('Form submitted!');
+  });
+  
